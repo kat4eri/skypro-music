75672e5ec5a915aa194ec5f239103ec55f49e0f2
kat4eri <kat4eri@gmail.com>
2024-01-30 04:37:47 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="App">
+          <header>
+              Здесь скоро будет Skypro.Music
+          </header>
+      </div>
+  );
+}
+
+export default App;
