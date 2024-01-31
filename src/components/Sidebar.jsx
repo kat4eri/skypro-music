33c74f8f6137eec0bf0edf405f2235f2c780abbb
kat4eri <kat4eri@gmail.com>
2024-01-31 03:45:37 +0300

    promlem with img

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="main__sidebar sidebar">
+      <div className="sidebar__personal">
+        <p className="sidebar__personal-name">kate.chernenko</p>
+        <div className="sidebar__icon">
+          <svg alt="logout">
+            <use xlinkHref="img/icon/sprite.svg#logout"></use>
+          </svg>
+        </div>
+      </div>
+      <div className="sidebar__block">
+        <div className="sidebar__list">
+          <div className="sidebar__item">
+            <a className="sidebar__link" href="profile.social.twitter">
+              <img
+                className="sidebar__img"
+                src="img/playlist01.png"
+                alt="day's playlist"
+              />
+            </a>
+          </div>
+          <div className="sidebar__item">
+            <a className="sidebar__link" href="profile.social.twitter">
+              <img
+                className="sidebar__img"
+                src="img/playlist03.png"
+                alt="day's playlist"
+              />
+            </a>
+          </div>
+          <div className="sidebar__item">
+            <a className="sidebar__link" href="profile.social.twitter">
+              <img
+                className="sidebar__img"
+                src="img/playlist03.png"
+                alt="day's playlist"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
